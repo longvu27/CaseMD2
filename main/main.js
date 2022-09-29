31437074f0c280c9b55fb1699a3f86828f2c4d76
unknown <you@example.com>
2022-09-29 10:53:08 +0700

    done

--- main/main.js
+++ main/main.js
@@ -10,11 +10,11 @@ var listAccount = new manageAccount_1.ManageAccount();
 var arrManageAlbum = [];
 var userCheck = null;
 function menuLogin() {
-    var menu = "------Menu-----\n1.Dang nhap\n2.Dang ky";
+    var menu = "------Menu-----\n1.\u0110\u0103ng Nh\u1EADp\n2.\u0110\u0103ng K\u00FD";
     var choice;
     do {
         console.log(menu);
-        choice = +input.question('Nhap lua chon cua ban: ');
+        choice = +input.question("Nhap lua chon cua ban: ");
         switch (choice) {
             case 1:
                 login();
@@ -22,36 +22,39 @@ function menuLogin() {
             case 2:
                 signUp();
                 break;
+            default: {
+                console.log("!!!B\u1EA1n v\u1EEBa nh\u1EADp c\u00E1i g\u00EC \u0111\u1EA5y!!!");
+            }
         }
     } while (choice != 0);
 }
 function login() {
     console.log("<<< B\u1EA1n \u0110ang \u1EDE Ph\u1EA7n \u0110\u0103ng Nh\u1EADp >>>");
-    console.log("B\u1EA1n ph\u1EA3i nh\u1EADp id \u0111\u1EE7 5 ch\u1EEF s\u1ED1 v\u00E0 kh\u00F4ng c\u00F3 ch\u1EEF c\u00E1i v\u00E0 k\u00FD t\u1EE9 \u0111\u1EB7c bi\u1EC7t n\u00E0o m\u1EDBi \u0111\u01B0\u1EE3c");
+    console.log("- B\u1EA1n ph\u1EA3i nh\u1EADp id \u0111\u1EE7 3 ch\u1EEF s\u1ED1 v\u00E0 kh\u00F4ng c\u00F3 ch\u1EEF c\u00E1i ho\u1EB7c k\u00FD t\u1EE9 \u0111\u1EB7c bi\u1EC7t n\u00E0o m\u1EDBi \u0111\u01B0\u1EE3c -");
     var id = input.question('Nhap id: ');
-    var idRegex = /^([0-9]){5,}$/;
+    var idRegex = /^([0-9]){3,}$/;
     var testId = idRegex.test(id);
     if (testId == false) {
         console.log(" Nh\u1EADp l\u1EA1i \u0111\u00EA");
         login();
     }
-    console.log("Nh\u1EADp t\u00EAn \u0111\u0103ng nh\u1EADp ph\u1EA3i \u0111\u1EE7 8 k\u00FD t\u1EF1, ch\u1EC9 ch\u1EE9a ch\u1EEF c\u00E1i th\u00F4i m\u1EDBi \u0111\u01B0\u1EE3c c\u01A1");
+    console.log("- Nh\u1EADp t\u00EAn \u0111\u0103ng nh\u1EADp ph\u1EA3i \u0111\u1EE7 5 k\u00FD t\u1EF1, ch\u1EC9 ch\u1EE9a ch\u1EEF c\u00E1i th\u00F4i m\u1EDBi \u0111\u01B0\u1EE3c c\u01A1 -");
     var name = input.question('Nhap ten dang nhap cua ban: ');
-    var nameRegex = /^([A-Za-z]){8,}$/;
+    var nameRegex = /^([A-Za-z]){5,}$/;
     var testName = nameRegex.test(name);
     if (testName == false) {
-        console.log(" \u0110\u00E3 b\u1EA3o nh\u1EADp cho \u0111\u00FAng r\u1ED3i kh\u00F4ng nghe, nh\u1EADp l\u1EA1i c\u1EA3 Id nh\u00E9");
+        console.log("- \u0110\u00E3 b\u1EA3o nh\u1EADp cho \u0111\u00FAng r\u1ED3i kh\u00F4ng nghe, nh\u1EADp l\u1EA1i c\u1EA3 Id nh\u00E9 -");
         login();
     }
-    console.log(" Ch\u1EC9 \u0111\u01B0\u1EE3c nh\u1EADp s\u1ED1 th\u00F4i, 8 s\u1ED1 nh\u00E9");
+    console.log("- Ch\u1EC9 \u0111\u01B0\u1EE3c nh\u1EADp s\u1ED1 th\u00F4i, 4 s\u1ED1 nh\u00E9 -");
     var password = input.question('Nhap mat khau: ');
-    var passwordRegex = /^([0-9]){8,}$/;
+    var passwordRegex = /^([0-9]){4,}$/;
     var testPass = passwordRegex.test(password);
     if (testPass == false) {
-        console.log('Nhập sai rồi, nhập lại từ đầu nhé, ai bảo nhập sai chiiiiii');
+        console.log('- Nhập sai rồi, nhập lại từ đầu nhé, ai bảo nhập sai chiiiiii -');
     }
     if (listAccount.listAccount.length == 0) {
-        console.log('<<<Dang nhap that bai, vui long nhap lai hoac tao tai khoan moi>>>');
+        console.log('!!!Dang nhap that bai, vui long nhap lai hoac tao tai khoan moi!!!');
     }
     for (var i = 0; i < listAccount.listAccount.length; i++) {
         if (listAccount.findByName(name, password) == i) {
@@ -60,18 +63,18 @@ function login() {
             menuAlbum();
         }
         else {
-            console.log('<<<Dang nhap that bai, vui long nhap lai hoac tao tai khoan moi>>>');
+            console.log('!!!Dang nhap that bai, vui long nhap lai hoac tao tai khoan moi!!!');
         }
     }
 }
 function signUp() {
     console.log("<<<B\u1EA1n \u0110ang \u1EDE Ph\u1EA7n \u0110\u0103ng K\u00FD>>>");
-    console.log("B\u1EA1n ph\u1EA3i nh\u1EADp id \u0111\u1EE7 5 ch\u1EEF s\u1ED1 v\u00E0 kh\u00F4ng c\u00F3 ch\u1EEF c\u00E1i v\u00E0 k\u00FD t\u1EE9 \u0111\u1EB7c bi\u1EC7t n\u00E0o m\u1EDBi \u0111\u01B0\u1EE3c");
+    console.log("- B\u1EA1n ph\u1EA3i nh\u1EADp id \u0111\u1EE7 3 ch\u1EEF s\u1ED1 v\u00E0 kh\u00F4ng c\u00F3 ch\u1EEF c\u00E1i ho\u1EB7c k\u00FD t\u1EE9 \u0111\u1EB7c bi\u1EC7t n\u00E0o m\u1EDBi \u0111\u01B0\u1EE3c -");
     var id = input.question('Nhap id: ');
-    var idRegex = /^([0-9]){5,}$/;
+    var idRegex = /^([0-9]){3,}$/;
     var testId = idRegex.test(id);
     if (testId == false) {
-        console.log(" Nh\u1EADp l\u1EA1i \u0111\u00EA");
+        console.log("- Nh\u1EADp l\u1EA1i \u0111\u00EA -");
         login();
     }
     for (var i = 0; i < listAccount.listAccount.length; i++) {
@@ -82,26 +85,27 @@ function signUp() {
         else {
         }
     }
-    console.log("Nh\u1EADp t\u00EAn \u0111\u0103ng nh\u1EADp ph\u1EA3i \u0111\u1EE7 8 k\u00FD t\u1EF1, ch\u1EC9 ch\u1EE9a ch\u1EEF c\u00E1i th\u00F4i m\u1EDBi \u0111\u01B0\u1EE3c c\u01A1");
+    console.log("- Nh\u1EADp t\u00EAn \u0111\u0103ng nh\u1EADp ph\u1EA3i \u0111\u1EE7 5 k\u00FD t\u1EF1, ch\u1EC9 ch\u1EE9a ch\u1EEF c\u00E1i th\u00F4i m\u1EDBi \u0111\u01B0\u1EE3c c\u01A1 -");
     var name = input.question('Nhap ten dang nhap cua ban: ');
-    var nameRegex = /^([A-Za-z]){8,}$/;
+    var nameRegex = /^([A-Za-z]){5,}$/;
     var testName = nameRegex.test(name);
     if (testName == false) {
-        console.log(" \u0110\u00E3 b\u1EA3o nh\u1EADp cho \u0111\u00FAng r\u1ED3i kh\u00F4ng nghe, nh\u1EADp l\u1EA1i c\u1EA3 Id nh\u00E9");
+        console.log("- \u0110\u00E3 b\u1EA3o nh\u1EADp cho \u0111\u00FAng r\u1ED3i kh\u00F4ng nghe, nh\u1EADp l\u1EA1i c\u1EA3 Id nh\u00E9 -");
         login();
     }
-    console.log(" Ch\u1EC9 \u0111\u01B0\u1EE3c nh\u1EADp s\u1ED1 th\u00F4i, 8 s\u1ED1 nh\u00E9");
+    console.log("- Ch\u1EC9 \u0111\u01B0\u1EE3c nh\u1EADp s\u1ED1 th\u00F4i, 4 s\u1ED1 nh\u00E9 -");
     var password = input.question('Nhap mat khau: ');
-    var passwordRegex = /^([0-9]){8,}$/;
+    var passwordRegex = /^([0-9]){4,}$/;
     var testPass = passwordRegex.test(password);
     if (testPass == false) {
-        console.log('Nhập sai rồi, nhập lại từ đầu nhé, ai bảo nhập sai chiiiiii');
+        console.log('- Nhập sai rồi, nhập lại từ đầu nhé, ai bảo nhập sai chiiiiii -');
     }
     var newUser = new account_1.Account(id, name, password);
     listAccount.add(newUser);
     var manageAlbum = new manageAlbum_1.ManageAlbum(newUser);
     arrManageAlbum.push(manageAlbum);
-    console.log('do dai mang acc: ' + listAccount.listAccount.length);
+    console.log("<<<\u0110\u0103ng K\u00FD Th\u00E0nh C\u00F4ng>>>");
+    // console.log('do dai mang acc: ' + listAccount.listAccount.length);
 }
 function menuAlbum() {
     var manageAlbumOneUser;
@@ -110,7 +114,7 @@ function menuAlbum() {
             manageAlbumOneUser = arrManageAlbum[i];
         }
     }
-    var menu = "-----Menu Album-----\n1.Them Album\n2.Chon Album\n3.Xoa Album\n4.Sua Album\n5.Thoat ";
+    var menu = "-----Menu Album-----\n1.Them Album\n2.Chon Album\n3.Xoa Album\n4.Sua Album\n5.Dang Xuat ";
     var choice;
     do {
         console.log(menu);
@@ -131,6 +135,9 @@ function menuAlbum() {
             case 5:
                 menuLogin();
                 break;
+            default: {
+                console.log("!!!B\u1EA1n v\u1EEBa nh\u1EADp c\u00E1i g\u00EC \u0111\u1EA5y!!!");
+            }
         }
     } while (choice != 0);
 }
@@ -147,7 +154,7 @@ function showAlbumOfUser(manageAlbumOneUser) {
         menuAlbum();
     }
     for (var i = 0; i < manageAlbumOneUser.listAlbum.length; i++) {
-        console.log("STT: ".concat(i + 1, " --- T\u00EAn Album: ").concat(manageAlbumOneUser.listAlbum[i].name));
+        console.log("STT: ".concat(i + 1, " - T\u00EAn Album: ").concat(manageAlbumOneUser.listAlbum[i].name));
     }
     var choiceAlbum = +input.question('Nhap stt ban Album ban muon: ');
     var menu = "---Menu Bai Hat---\n1.Them Bai Hat\n2.Chon Bai Hat De Tuong Tac\n3.Tro Ve";
@@ -164,33 +171,45 @@ function showAlbumOfUser(manageAlbumOneUser) {
                 break;
             case 3:
                 menuAlbum();
+                break;
+            // case 4:
+            //     searchSong(manageAlbumOneUser);
+            //     break;
+            default: {
+                console.log("!!!B\u1EA1n v\u1EEBa nh\u1EADp c\u00E1i g\u00EC \u0111\u1EA5y!!!");
+            }
         }
     } while (choice != 0);
 }
 function deleteAlbum(manageAlbumOneUser) {
     console.log("<<<B\u1EA1n \u0110ang \u1EDE Ph\u1EA7n X\u00F3a Album>>>");
     if (manageAlbumOneUser.listAlbum.length == 0) {
-        console.log('Khong co Album nao');
+        console.log('!!!Khong co Album nao!!!');
         menuAlbum();
+    }
+    for (var i = 0; i < manageAlbumOneUser.listAlbum.length; i++) {
+        console.log("STT: ".concat(i + 1, " - T\u00EAn Album: ").concat(manageAlbumOneUser.listAlbum[i].name));
     }
     var name = input.question('Nhap ten Album ban muon xoa: ');
     manageAlbumOneUser["delete"](name);
-    console.log(" B\u1EA1n \u0110\u00E3 X\u00F3a Album: ".concat(name));
+    console.log("- B\u1EA1n \u0110\u00E3 X\u00F3a Album: ".concat(name));
 }
 function editAlbum(manageAlbumOneUser) {
     console.log("<<<B\u1EA1n \u0110ang \u1EDE Ph\u1EA7n S\u1EEDa Album>>>");
     if (manageAlbumOneUser.listAlbum.length == 0) {
-        console.log('Khong co Album nao');
+        console.log('!!!Khong co Album nao!!!');
         menuAlbum();
     }
+    // showAlbumOfUser(manageAlbumOneUser);
     var name = input.question('Nhap ten album muon sua: ');
     if (manageAlbumOneUser.findByName(name) == -1) {
-        console.log('Khong có Album nay');
+        console.log('!!!Khong có Album nay!!!');
     }
     else {
         var newName = input.question('Nhap ten moi: ');
         var newAlbum = new album_1.Album(newName);
-        manageAlbumOneUser.edit(newName, newAlbum);
+        manageAlbumOneUser.edit(name, newAlbum);
+        // console.log('aaaaaa')
     }
 }
 function addSong(manageAlbumOneUser, choiceAlbum) {
@@ -208,10 +227,10 @@ function showSong(manageAlbumOneUser, choiceAlbum) {
         showAlbumOfUser(manageAlbumOneUser);
     }
     for (var i = 0; i < manageAlbumOneUser.listAlbum[choiceAlbum - 1].listSong.length; i++) {
-        console.log("STT: ".concat(i + 1, " --- Ten bai hat: ").concat(manageAlbumOneUser.listAlbum[choiceAlbum - 1].listSong[i].name));
+        console.log("STT: ".concat(i + 1, " - Ten bai hat: ").concat(manageAlbumOneUser.listAlbum[choiceAlbum - 1].listSong[i].name, " - S\u00E1ng T\u00E1c: ").concat(manageAlbumOneUser.listAlbum[choiceAlbum - 1].listSong[i].author, " - Ca S\u0129: ").concat(manageAlbumOneUser.listAlbum[choiceAlbum - 1].listSong[i].singer));
     }
     var choiceSong = +input.question('Nhap stt bai hat ban muon tuong tac: ');
-    var menu = "---Menu Tuong Tac Voi Bai Hat---\n1.Sua\n2.Xoa\n3.Tro Ve";
+    var menu = "---Menu Tuong Tac Voi Bai Hat---\n1.Sua\n2.Xoa\n3.Tro Ve\n";
     var choice;
     do {
         console.log(menu);
@@ -225,6 +244,10 @@ function showSong(manageAlbumOneUser, choiceAlbum) {
                 break;
             case 3:
                 menuAlbum();
+                break;
+            default: {
+                console.log("!!!B\u1EA1n v\u1EEBa nh\u1EADp c\u00E1i g\u00EC \u0111\u1EA5y!!!");
+            }
         }
     } while (choice != 0);
 }
@@ -240,8 +263,30 @@ function editSong(manageAlbumOneUser, choiceAlbum, choiceSong) {
 }
 function deleteSong(manageAlbumOneUser, choiceAlbum, choiceSong) {
     console.log("<<<B\u1EA1n \u0110ang \u1EDE Ph\u1EA7n X\u00F3a B\u00E0i H\u00E1t>>>");
-    console.log("<<<B\u1EA1n \u0110\u00E3 X\u00F3a B\u00E0i H\u00E1t: ".concat(manageAlbumOneUser.listAlbum[choiceAlbum - 1].listSong[choiceSong - 1].name));
+    console.log("<<<B\u1EA1n \u0110\u00E3 X\u00F3a B\u00E0i H\u00E1t: ".concat(manageAlbumOneUser.listAlbum[choiceAlbum - 1].listSong[choiceSong - 1].name, ">>>"));
     manageAlbumOneUser.listAlbum[choiceAlbum - 1].listSong.splice(choiceSong - 1, 1);
     showAlbumOfUser(manageAlbumOneUser);
 }
+// function searchSong(manageAlbumOneUser: ManageAlbum, choiceAlbum ) {
+//     let search = input.question('Nhap');
+//     let name = new RegExp(search);
+//
+//     if(search.length == 0) {
+//         console.log(` Chưa nhập gì`)}
+//     else{
+//         let sum = 0;
+//         for (let i = 0; i < manageAlbumOneUser.listAlbum[choiceAlbum-1].listSong.length; i++) {
+//             let test = name.test(manageAlbumOneUser.listAlbum[choiceAlbum-1].listSong[i].name);
+//             if(test == true) {
+//                 sum++;
+//                 console.log(` Tên bài hát: ${manageAlbumOneUser.listAlbum[choiceAlbum-1].listSong[i].name}`)
+//             }
+//
+//         }if(sum==0) {
+//             console.log(`Khong có`);
+//         }
+//     }
+//     // console.log(manageAlbumOneUser.listAlbum.filter(item => item.name.includes(search)))
+//
+// }
 menuLogin();
--- main/main.ts
+++ main/main.ts
@@ -6,7 +6,6 @@ import {ManageSong} from "../sevice/manageSong";
 import {Song} from "../model/song";
 import {ManageSongOfAlbum} from "../sevice/manageSongOfAlbum";
 
-
 let input = require('readline-sync');
 
 let listAccount: ManageAccount = new ManageAccount();
@@ -16,11 +15,11 @@ let arrManageAlbum: ManageAlbum[] = [];
 let userCheck = null;
 
 function menuLogin() {
-    let menu = `------Menu-----\n1.Dang nhap\n2.Dang ky`;
+    let menu = `------Menu-----\n1.Đăng Nhập\n2.Đăng Ký`;
     let choice;
     do {
         console.log(menu);
-        choice = +input.question('Nhap lua chon cua ban: ');
+        choice = +input.question(`Nhap lua chon cua ban: `);
         switch (choice) {
             case 1:
                 login()
@@ -28,41 +27,44 @@ function menuLogin() {
             case 2:
                 signUp()
                 break;
+            default: {
+                console.log(`!!!Bạn vừa nhập cái gì đấy!!!`);
+            }
         }
     } while (choice != 0);
 }
 
 function login() {
     console.log(`<<< Bạn Đang Ở Phần Đăng Nhập >>>`);
-    console.log(`Bạn phải nhập id đủ 5 chữ số và không có chữ cái và ký tứ đặc biệt nào mới được`)
+    console.log(`- Bạn phải nhập id đủ 3 chữ số và không có chữ cái hoặc ký tứ đặc biệt nào mới được -`);
     let id = input.question('Nhap id: ');
-    let idRegex = /^([0-9]){5,}$/;
+    let idRegex = /^([0-9]){3,}$/;
     let testId = idRegex.test(id);
-    if(testId ==false) {
+    if (testId == false) {
         console.log(` Nhập lại đê`);
         login();
     }
 
-    console.log(`Nhập tên đăng nhập phải đủ 8 ký tự, chỉ chứa chữ cái thôi mới được cơ`);
+    console.log(`- Nhập tên đăng nhập phải đủ 5 ký tự, chỉ chứa chữ cái thôi mới được cơ -`);
     let name = input.question('Nhap ten dang nhap cua ban: ');
-    let nameRegex = /^([A-Za-z]){8,}$/;
+    let nameRegex = /^([A-Za-z]){5,}$/;
     let testName = nameRegex.test(name);
-    if(testName == false) {
-        console.log(` Đã bảo nhập cho đúng rồi không nghe, nhập lại cả Id nhé`);
+    if (testName == false) {
+        console.log(`- Đã bảo nhập cho đúng rồi không nghe, nhập lại cả Id nhé -`);
         login();
     }
 
-    console.log(` Chỉ được nhập số thôi, 8 số nhé`);
+    console.log(`- Chỉ được nhập số thôi, 4 số nhé -`);
     let password = input.question('Nhap mat khau: ');
-    let passwordRegex = /^([0-9]){8,}$/;
+    let passwordRegex = /^([0-9]){4,}$/;
     let testPass = passwordRegex.test(password);
-    if(testPass == false) {
-        console.log('Nhập sai rồi, nhập lại từ đầu nhé, ai bảo nhập sai chiiiiii');
+    if (testPass == false) {
+        console.log('- Nhập sai rồi, nhập lại từ đầu nhé, ai bảo nhập sai chiiiiii -');
     }
 
 
     if (listAccount.listAccount.length == 0) {
-        console.log('<<<Dang nhap that bai, vui long nhap lai hoac tao tai khoan moi>>>');
+        console.log('!!!Dang nhap that bai, vui long nhap lai hoac tao tai khoan moi!!!');
     }
 
     for (let i = 0; i < listAccount.listAccount.length; i++) {
@@ -71,19 +73,19 @@ function login() {
             userCheck = listAccount.listAccount[i];
             menuAlbum();
         } else {
-            console.log('<<<Dang nhap that bai, vui long nhap lai hoac tao tai khoan moi>>>');
+            console.log('!!!Dang nhap that bai, vui long nhap lai hoac tao tai khoan moi!!!');
         }
     }
 }
 
 function signUp() {
     console.log(`<<<Bạn Đang Ở Phần Đăng Ký>>>`);
-    console.log(`Bạn phải nhập id đủ 5 chữ số và không có chữ cái và ký tứ đặc biệt nào mới được`)
+    console.log(`- Bạn phải nhập id đủ 3 chữ số và không có chữ cái hoặc ký tứ đặc biệt nào mới được -`);
     let id = input.question('Nhap id: ');
-    let idRegex = /^([0-9]){5,}$/;
+    let idRegex = /^([0-9]){3,}$/;
     let testId = idRegex.test(id);
-    if(testId ==false) {
-        console.log(` Nhập lại đê`);
+    if (testId == false) {
+        console.log(`- Nhập lại đê -`);
         login();
     }
 
@@ -95,29 +97,29 @@ function signUp() {
         }
     }
 
-    console.log(`Nhập tên đăng nhập phải đủ 8 ký tự, chỉ chứa chữ cái thôi mới được cơ`);
+    console.log(`- Nhập tên đăng nhập phải đủ 5 ký tự, chỉ chứa chữ cái thôi mới được cơ -`);
     let name = input.question('Nhap ten dang nhap cua ban: ');
-    let nameRegex = /^([A-Za-z]){8,}$/;
+    let nameRegex = /^([A-Za-z]){5,}$/;
     let testName = nameRegex.test(name);
-    if(testName == false) {
-        console.log(` Đã bảo nhập cho đúng rồi không nghe, nhập lại cả Id nhé`);
+    if (testName == false) {
+        console.log(`- Đã bảo nhập cho đúng rồi không nghe, nhập lại cả Id nhé -`);
         login();
     }
 
-    console.log(` Chỉ được nhập số thôi, 8 số nhé`);
+    console.log(`- Chỉ được nhập số thôi, 4 số nhé -`);
     let password = input.question('Nhap mat khau: ');
-    let passwordRegex = /^([0-9]){8,}$/;
+    let passwordRegex = /^([0-9]){4,}$/;
     let testPass = passwordRegex.test(password);
-    if(testPass == false) {
-        console.log('Nhập sai rồi, nhập lại từ đầu nhé, ai bảo nhập sai chiiiiii');
+    if (testPass == false) {
+        console.log('- Nhập sai rồi, nhập lại từ đầu nhé, ai bảo nhập sai chiiiiii -');
     }
-
     let newUser = new Account(id, name, password);
     listAccount.add(newUser);
 
     let manageAlbum = new ManageAlbum(newUser);
     arrManageAlbum.push(manageAlbum);
-    console.log('do dai mang acc: ' + listAccount.listAccount.length);
+    console.log(`<<<Đăng Ký Thành Công>>>`)
+    // console.log('do dai mang acc: ' + listAccount.listAccount.length);
 }
 
 function menuAlbum() {
@@ -127,7 +129,7 @@ function menuAlbum() {
             manageAlbumOneUser = arrManageAlbum[i];
         }
     }
-    let menu = `-----Menu Album-----\n1.Them Album\n2.Chon Album\n3.Xoa Album\n4.Sua Album\n5.Thoat `;
+    let menu = `-----Menu Album-----\n1.Them Album\n2.Chon Album\n3.Xoa Album\n4.Sua Album\n5.Dang Xuat `;
     let choice;
 
     do {
@@ -149,12 +151,15 @@ function menuAlbum() {
             case 5:
                 menuLogin();
                 break;
+            default: {
+                console.log(`!!!Bạn vừa nhập cái gì đấy!!!`)
+            }
         }
     } while (choice != 0)
 }
 
 function addAlbum(manageAlbumOneUser: ManageAlbum) {
-    console.log(`<<<Bạn Đang Ở Phần Thêm Album>>>`)
+    console.log(`<<<Bạn Đang Ở Phần Thêm Album>>>`);
     let name = input.question('Nhap ten Album: ');
     let album = new Album(name);
     manageAlbumOneUser.add(album);
@@ -167,52 +172,68 @@ function showAlbumOfUser(manageAlbumOneUser: ManageAlbum) {
         menuAlbum();
     }
     for (let i = 0; i < manageAlbumOneUser.listAlbum.length; i++) {
-        console.log(`STT: ${i + 1} --- Tên Album: ${manageAlbumOneUser.listAlbum[i].name}`)
+        console.log(`STT: ${i + 1} - Tên Album: ${manageAlbumOneUser.listAlbum[i].name}`)
     }
 
-    let choiceAlbum = +input.question('Nhap stt ban Album ban muon: ')
-    let menu = `---Menu Bai Hat---\n1.Them Bai Hat\n2.Chon Bai Hat De Tuong Tac\n3.Tro Ve`
-    let choice
+    let choiceAlbum = +input.question('Nhap stt ban Album ban muon: ');
+    let menu = `---Menu Bai Hat---\n1.Them Bai Hat\n2.Chon Bai Hat De Tuong Tac\n3.Tro Ve`;
+    let choice;
     do {
         console.log(menu);
         choice = +input.question('Nhap lua chon cua ban: ');
         switch (choice) {
             case 1:
-                addSong(manageAlbumOneUser, choiceAlbum)
+                addSong(manageAlbumOneUser, choiceAlbum);
                 break;
             case 2:
-                showSong(manageAlbumOneUser, choiceAlbum)
+                showSong(manageAlbumOneUser, choiceAlbum);
                 break;
             case 3:
                 menuAlbum();
+                break;
+            // case 4:
+            //     searchSong(manageAlbumOneUser);
+            //     break;
+            default: {
+                console.log(`!!!Bạn vừa nhập cái gì đấy!!!`);
+            }
         }
     } while (choice != 0);
 }
 
 function deleteAlbum(manageAlbumOneUser: ManageAlbum) {
-    console.log(`<<<Bạn Đang Ở Phần Xóa Album>>>`)
+    console.log(`<<<Bạn Đang Ở Phần Xóa Album>>>`);
     if (manageAlbumOneUser.listAlbum.length == 0) {
-        console.log('Khong co Album nao');
+        console.log('!!!Khong co Album nao!!!');
         menuAlbum();
     }
+
+    for (let i = 0; i < manageAlbumOneUser.listAlbum.length; i++) {
+        console.log(`STT: ${i + 1} - Tên Album: ${manageAlbumOneUser.listAlbum[i].name}`);
+    }
+
     let name = input.question('Nhap ten Album ban muon xoa: ');
     manageAlbumOneUser.delete(name);
-    console.log(` Bạn Đã Xóa Album: ${name}`)
+    console.log(`- Bạn Đã Xóa Album: ${name}`);
 }
 
 function editAlbum(manageAlbumOneUser: ManageAlbum) {
     console.log(`<<<Bạn Đang Ở Phần Sửa Album>>>`);
     if (manageAlbumOneUser.listAlbum.length == 0) {
-        console.log('Khong co Album nao');
+        console.log('!!!Khong co Album nao!!!');
         menuAlbum();
     }
+
+    // showAlbumOfUser(manageAlbumOneUser);
+
     let name = input.question('Nhap ten album muon sua: ');
     if (manageAlbumOneUser.findByName(name) == -1) {
-        console.log('Khong có Album nay');
+        console.log('!!!Khong có Album nay!!!');
     } else {
         let newName = input.question('Nhap ten moi: ');
         let newAlbum = new Album(newName);
-        manageAlbumOneUser.edit(newName, newAlbum)
+        manageAlbumOneUser.edit(name, newAlbum);
+        // console.log('aaaaaa')
     }
 }
 
@@ -229,17 +250,17 @@ function addSong(manageAlbumOneUser: ManageAlbum, choiceAlbum) {
 function showSong(manageAlbumOneUser: ManageAlbum, choiceAlbum) {
     console.log(`<<<Bạn Đang Ở Phần Tương Tác Với Bài Hát>>>`);
 
-    if(manageAlbumOneUser.listAlbum[choiceAlbum-1].listSong.length == 0) {
+    if (manageAlbumOneUser.listAlbum[choiceAlbum - 1].listSong.length == 0) {
         console.log('<<<Khong co bai hat nao>>>');
         showAlbumOfUser(manageAlbumOneUser);
     }
 
     for (let i = 0; i < manageAlbumOneUser.listAlbum[choiceAlbum - 1].listSong.length; i++) {
-        console.log(`STT: ${i + 1} --- Ten bai hat: ${manageAlbumOneUser.listAlbum[choiceAlbum - 1].listSong[i].name}`);
+        console.log(`STT: ${i + 1} - Ten bai hat: ${manageAlbumOneUser.listAlbum[choiceAlbum - 1].listSong[i].name} - Sáng Tác: ${manageAlbumOneUser.listAlbum[choiceAlbum - 1].listSong[i].author} - Ca Sĩ: ${manageAlbumOneUser.listAlbum[choiceAlbum - 1].listSong[i].singer}`);
     }
     let choiceSong = +input.question('Nhap stt bai hat ban muon tuong tac: ');
 
-    let menu = `---Menu Tuong Tac Voi Bai Hat---\n1.Sua\n2.Xoa\n3.Tro Ve`;
+    let menu = `---Menu Tuong Tac Voi Bai Hat---\n1.Sua\n2.Xoa\n3.Tro Ve\n`;
     let choice;
     do {
         console.log(menu);
@@ -253,6 +274,10 @@ function showSong(manageAlbumOneUser: ManageAlbum, choiceAlbum) {
                 break;
             case 3:
                 menuAlbum();
+                break;
+            default: {
+                console.log(`!!!Bạn vừa nhập cái gì đấy!!!`)
+            }
         }
     } while (choice != 0)
 }
@@ -274,12 +299,34 @@ function editSong(manageAlbumOneUser: ManageAlbum, choiceAlbum, choiceSong) {
 function deleteSong(manageAlbumOneUser: ManageAlbum, choiceAlbum, choiceSong) {
     console.log(`<<<Bạn Đang Ở Phần Xóa Bài Hát>>>`);
 
-    console.log(`<<<Bạn Đã Xóa Bài Hát: ${manageAlbumOneUser.listAlbum[choiceAlbum - 1].listSong[choiceSong - 1].name}`);
+    console.log(`<<<Bạn Đã Xóa Bài Hát: ${manageAlbumOneUser.listAlbum[choiceAlbum - 1].listSong[choiceSong - 1].name}>>>`);
 
     manageAlbumOneUser.listAlbum[choiceAlbum - 1].listSong.splice(choiceSong - 1, 1);
 
     showAlbumOfUser(manageAlbumOneUser);
 }
 
+// function searchSong(manageAlbumOneUser: ManageAlbum, choiceAlbum ) {
+//     let search = input.question('Nhap');
+//     let name = new RegExp(search);
+//
+//     if(search.length == 0) {
+//         console.log(` Chưa nhập gì`)}
+//     else{
+//         let sum = 0;
+//         for (let i = 0; i < manageAlbumOneUser.listAlbum[choiceAlbum-1].listSong.length; i++) {
+//             let test = name.test(manageAlbumOneUser.listAlbum[choiceAlbum-1].listSong[i].name);
+//             if(test == true) {
+//                 sum++;
+//                 console.log(` Tên bài hát: ${manageAlbumOneUser.listAlbum[choiceAlbum-1].listSong[i].name}`)
+//             }
+//
+//         }if(sum==0) {
+//             console.log(`Khong có`);
+//         }
+//     }
+//     // console.log(manageAlbumOneUser.listAlbum.filter(item => item.name.includes(search)))
+//
+// }
 
 menuLogin();
